71bba9e006ca0bf0cb9241b2a731ee29396a2ec3
a <sqlchan@163.com>
2017-06-28 14:29:22 +0800

    高级课-02 over

---
+++ src/main/java/com/sqlchan/wenda/model/User.java
@@ -0,0 +1,20 @@
+package com.sqlchan.wenda.model;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
